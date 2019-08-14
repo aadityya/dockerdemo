15888f9b834b2475911838636ae6cd54a09bdb1e
aditya <aadityya@gmail.com>
2019-08-14 19:47:31 -0400

    commit2

--- src/main/java/com/example/springbootdemo/HelloController.java
+++ src/main/java/com/example/springbootdemo/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String sayHello() {
-        return "Hi!";
+        return "Hi-1!";
     }
 }
